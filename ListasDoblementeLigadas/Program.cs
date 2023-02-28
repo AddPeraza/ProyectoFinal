418cace797cad27f78ec707f9f5c21232ed7630e
adderly.peraza <addperaza@gmail.com>
2023-02-28 11:26:51 -0700

    Creando la salida en pantalla de ListasDoblementeLigadas

--- ListasDoblementeLigadas/Program.cs
+++ ListasDoblementeLigadas/Program.cs
@@ -10,6 +10,33 @@ namespace ListasDoblementeLigadas
     {
         static void Main(string[] args)
         {
+            Lista miLista = new Lista();
+
+            Console.WriteLine("Lista vacía: " + miLista.ValidaVacio()); // debería imprimir true
+
+            miLista.AgregarFinal("Nodo 1");
+            miLista.AgregarFinal("Nodo 2");
+            miLista.AgregarFinal("Nodo 3");
+
+            Console.WriteLine(miLista.Recorrer()); // debería imprimir "nodo 1\nodo 2\nodo 3\n"
+
+            miLista.AgregarInicio("Nodo 0");
+
+            Console.WriteLine(miLista.Recorrer()); // debería imprimir nodo 0\nodo 1\nodo 2\nodo 3\n
+
+            Console.WriteLine("Buscar 'Nodo 2': " + miLista.Buscar("Nodo 2")); 
+
+            miLista.EliminarNodo("Nodo 1");
+
+            Console.WriteLine(miLista.Recorrer()); // debería imprimir los nodos 
+
+            miLista.VaciarLista();
+
+            Console.WriteLine("Lista vacía: " + miLista.ValidaVacio()); // debería imprimir true
+
+            Console.ReadKey();
         }
     }
+    
 }
+
